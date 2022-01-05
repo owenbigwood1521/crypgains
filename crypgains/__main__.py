55fb2bebc9ef207479203b417d6158f87a737324
Owen Bigwood <owenbigwood1521@gmail.com>
2022-01-05 14:44:26 +0000

    add csv to ignore

--- crypgains/__main__.py
+++ crypgains/__main__.py
@@ -1,7 +1,8 @@
 from crypgains.prices import update_price_histories, update_price_current
 from crypgains.portfolio.portfolio import calculate_total
 
-if __name__ == '__main__':
+if __name__ == '__main__':    
     update_price_histories.run()
     update_price_current.run()
     calculate_total('crypgains/resources/portfolio.yaml')
+
